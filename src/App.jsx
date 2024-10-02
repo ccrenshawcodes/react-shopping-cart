0a3d38b06ac880348246d2724fbb1a195091440f
Catherine Crenshaw <55335367+ccrenshawcodes@users.noreply.github.com>
2024-10-02 17:51:23 -0600

    Wire up routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
+//  external dependencies
+import { Outlet } from 'react-router-dom'
+
+import NavBar from './components/NavBar'
+
 function App() {
   return (
     <>
-      <p>Hello, world!</p>
+      <NavBar />
+      <Outlet />
     </>
   )
 }
 
 export default App
 
+/* 
+  NEXT STEPS:
+    1. Set up a router - done :) 
+    2. Create basic content for App/nav, Home, Shop and Cart - done :) 
+    3. Explore the fake store API
+    4. Figure out how I'm going to store state and fetch.
+    5. Style.
+*/
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
+//  external dependencies
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+//  relative dependencies
 import App from './App.jsx'
 import './index.css'
+import Shop from './components/Shop.jsx'
+import Cart from './components/Cart.jsx'
+import Home from './components/Home.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {index: true, element: <Home />},
+      {path: 'shop', element: <Shop />,},
+      {path: 'cart', element: <Cart />,},
+    ],
+  },
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
