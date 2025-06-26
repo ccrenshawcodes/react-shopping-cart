ba2c75572ecb21a726ddcf46c0221afd1c7b0e80
catherine <55335367+ccrenshawcodes@users.noreply.github.com>
2025-06-25 20:59:10 -0600

    add price calculation

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -5,23 +5,33 @@ import Card from "./Card";
 
 function Cart() {
   const [cartItems] = useOutletContext();
+  const cartIsNotEmpty = cartItems.length > 0;
 
-  const cartItemsArray =
-    cartItems.length > 0 ? (
-      cartItems.map((item) => (
-        <Card
-          itemName={item.itemName}
-          itemPrice={item.itemPrice}
-          itemImage={item.itemImage}
-          inCart={true}
-          key={item.itemId}
-          itemId={item.itemId}
-          itemQty={item.itemQty}
-        />
-      ))
-    ) : (
-      <p>You have not added anything to your cart yet.</p>
-    );
+  function calculatePrice() {
+    let total = 0;
+    if (cartIsNotEmpty) {
+      cartItems.map((item) => (total += item.itemPrice * item.itemQty));
+    }
+    return total.toFixed(2);
+  }
+
+  const totalPrice = calculatePrice();
+
+  const cartItemsArray = cartIsNotEmpty ? (
+    cartItems.map((item) => (
+      <Card
+        itemName={item.itemName}
+        itemPrice={item.itemPrice}
+        itemImage={item.itemImage}
+        inCart={true}
+        key={item.itemId}
+        itemId={item.itemId}
+        itemQty={item.itemQty}
+      />
+    ))
+  ) : (
+    <p>You have not added anything to your cart yet.</p>
+  );
 
   return (
     <div className="cart-page">
@@ -29,7 +39,7 @@ function Cart() {
         <h2>Your Cart</h2>
 
         <Link to="checkout">
-          <button>Checkout</button>
+          <button>Checkout {cartIsNotEmpty && ` - ${totalPrice}`}</button>
         </Link>
       </div>
       <div className="cart-items">{cartItemsArray}</div>
